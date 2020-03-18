6bba8baa37233f64c0f44b7535752c6c871f570d
simcha33 <36536375+simcha33@users.noreply.github.com>
2020-03-18 16:10:14 +0100

    punten systeem

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,23 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Asteroid : MonoBehaviour {
+public class Asteroid : MonoBehaviour
+{
     private Rigidbody2D rb;
 
     public bool held = false;
 
-    void Start() {
+    public bool inOrbit = false;
+
+
+
+    public float orbitDuration;
+
+    [HideInInspector] public float orbitTimer;
+
+    public float orbitSpeed;
+
+    public float value;
+
+    public GameObject tempPlanet;
+
+    PlayerScore _playerscore;
+
+
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+
+
     }
 
-    void OnCollisionEnter2D(Collision2D col) {
-        if(col.gameObject.tag == "HOOKSHOT") {
+    void Update()
+    {
+        OrbitAroundPlanet(); //Function orbits an astroid around a player planet 
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "HOOKSHOT")
+        {
             transform.position = col.transform.position;
-            rb.simulated = false;
+            //rb.simulated = false;
             col.gameObject.GetComponent<HookTip>().hookShot.CatchObject(gameObject);
+            //Debug.Log("grabbed");
+            held = true;
         }
-        else if(col.gameObject.tag == "PlayerPlanet") {
-            if(held) ;
+
+        //if the player just flies the resource straight in the planet instantly give him the points 
+        if (col.gameObject.tag == "PLAYERPLANET" && col.gameObject != null)
+        {
+            _playerscore = col.gameObject.GetComponent<PlayerScore>();
+            _playerscore.AddingResource(value);
+            gameObject.SetActive(false);
+            held = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //if the player throws the rock to his planet instead of flying there let it orbit for a bit          
+            if (col.gameObject.tag == "PLAYERPLANET")          
+                _playerscore = col.gameObject.GetComponent<PlayerScore>();    
+    }
+
+    void OrbitAroundPlanet()
+    {
+        if (!held)
+        {
+            if (inOrbit)
+            {
+                transform.RotateAround(tempPlanet.transform.position, Vector3.forward, orbitSpeed * Time.deltaTime);  //How to find the planet has to be reworked! but it rotates the astroid around the players planet                                                                                                                       //start a timer 
+                orbitTimer += Time.deltaTime;
+
+                if (orbitTimer >= orbitDuration)  //if the astroid has finished his time in the orbit collect the points 
+                {
+                    _playerscore.AddingResource(value);
+                    gameObject.SetActive(false);
+                    inOrbit = false;
+                    held = false; 
+                    orbitTimer = 0;
+                }
+                //lerp resource to the planet 
+                Debug.Log("rotate around planet");
+            }
         }
     }
 }
--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PlayerScore : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float currentScore;
+
+
+    public float resourceValue; 
+
+    public float maxScore;
+
+    public float minScore;
+
+    public bool resourceAdded;
+
+    public bool resourceInOrbit; 
+
+    public Text scoreText; 
+
+    public GameObject playerPlanet;
+
+    Asteroid _asteroid; 
+
     void Start()
     {
-        
+        currentScore = 0; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //AddingResource(); 
+        scoreText.text = currentScore.ToString("F0");
     }
-}
+
+    
+    public void AddingResource(float amount)
+    {
+        currentScore += amount; 
+        //playerPlanet.transform.localScale = new Vector2(2f, 2f );             
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //if the player throws the rock to his planet instead of flying there let it orbit for a bit
+            if (col.gameObject.tag == "Resource")
+            {
+                resourceInOrbit = true;
+                _asteroid = col.gameObject.GetComponent<Asteroid>();
+                _asteroid.inOrbit = true; 
+            }
+        }
+    }
