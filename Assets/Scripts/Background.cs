2ed6c84015e70ab02f54fbf88d6da389a1216ee8
Can Ur <canur11@gmail.com>
2020-04-01 11:52:26 +0200

    Pusheke

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour {
+    private Image src;
+
+    public bool parallax = false;
+
+    public float parallaxSpeed = 1f;
+
+    void Start() {
+        src = GetComponent<Image>();
+        if(!parallax) SetTexture(TextureSwitcher.GetCurrentTexturePack());
+    }
+
+    void Update() {
+        if(!parallax) transform.Rotate(Vector3.forward * 1 * Time.deltaTime);
+        else transform.Rotate(Vector3.forward * parallaxSpeed * Time.deltaTime);
+    }
+
+
+    public void SetTexture(TextureSwitcher.TexturePack elm) {
+        if(src == null) src = GetComponent<Image>();
+        if(src == null) return;
+        src.sprite = elm.Background.src;
+    }
+}
---
+++ Assets/Scripts/SunTextures.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SunTextures : MonoBehaviour {
+    	public Image src, glow;
+
+		void Start() {
+			var pack = TextureSwitcher.GetCurrentTexturePack().blackHole;
+			src.sprite = pack.src;
+			glow.sprite = pack.glow;
+			UpdateSize();
+		}
+
+		public void UpdateSize() {
+			src.SetNativeSize();
+			glow.SetNativeSize();
+		}
+}
