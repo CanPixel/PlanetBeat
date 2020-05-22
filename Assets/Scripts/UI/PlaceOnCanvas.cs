57313cdda17e7d13e79be55061d2599f6a46f2bf
Bart Slot <bartslot@gmail.com>
2020-05-22 12:08:27 +0200

    ALLES VERWIJDERD

--- Assets/Scripts/UI/PlaceOnCanvas.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlaceOnCanvas : MonoBehaviour {
-    void Start() {
-        transform.SetParent(GameObject.FindGameObjectWithTag("GAMEFIELD").transform, false);
-    }
-}
